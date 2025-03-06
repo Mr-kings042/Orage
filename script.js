82bc795d97f1c3c833b9bd85cd5d00684309c48a
Mr-kings042 <okohkings042@gmail.com>
2025-03-06 18:55:55 +0100

    changed all the background images and added new sections and changed the social media icons

--- script.js
+++ script.js
@@ -42,4 +42,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
   });
-  
+  // Back to Top Button
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
